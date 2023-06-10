e5a14eb5c115e7d89f52d16da9e618b5413b8090
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2023-06-09 19:26:25 -0600

    implementar bootstrap con sass

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
+  <button class="btn btn-primary"> primary</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-success"> success</button>
 </template>
 
 <script>
