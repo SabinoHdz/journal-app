03273729c3d4230ac96cab5a97bbd9aca5875495
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2023-06-12 17:51:44 -0600

    Estructura del DaybookLayout

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>Daybook Layout</h1>
+  <NavbarView/>
+  <div class="d-flex">
+    <div class="col-4">
+      <entry-list></entry-list>
+    </div>
+    <div class="col">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+import EntryList from '../components/EntryList.vue'
+export default {
+components:{
+    NavbarView:defineAsyncComponent(()=>import('../components/NavbarView.vue')),
+    EntryList
+}
+}
+</script>
+<style>
+
+</style>
